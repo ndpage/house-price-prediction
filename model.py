7fe14bfd8efed6b62e869866ee2f82e7da5e8f52
Nathan Page <33336363+ndpage@users.noreply.github.com>
2024-04-07 19:01:59 +0000

    Add house price data, fix script

--- model.py
+++ model.py
@@ -1,18 +1,29 @@
-from sklearn import LinearRegression
+from sklearn.linear_model import LinearRegression
 import pandas as pd
 
 # Load the dataset (replace this with your dataset)
-data = pd.read_csv("house_price_index.csv")
+data = pd.read_csv("house_prices.csv")
 
 # Split the data into features (X) and target variable (y)
-X = data.drop(columns=['Price ($)'])
-y = data['Price ($)']
+'''
+        |    /
+ Price  |  _/
+        | /
+        |/____________
+            Features
+'''
+
+# area,bedrooms,bathrooms,stories,mainroad,guestroom,basement,hotwaterheating,airconditioning,parking,prefarea,furnishingstatus
+X = data.drop(columns=['price','mainroad','guestroom','basement','hotwaterheating','airconditioning','parking','prefarea','furnishingstatus'])
+y = data['price']
 
 # Initialize and train the linear regression model
 model = LinearRegression()
 model.fit(X, y)
 
 # Example prediction for a new house
-new_house = [[2500, 3, 2, 'Suburb']]  # Example features for a new house
+# area,bedrooms,bathrooms,stories,mainroad,guestroom,basement,hotwaterheating,airconditioning,parking,prefarea,furnishingstatus
+new_house = [[2500, 3, 2, 1]]  # Example features for a new house
 predicted_price = model.predict(new_house)
 print("Predicted price for the new house:", predicted_price[0])
+
