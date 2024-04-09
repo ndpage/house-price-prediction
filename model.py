a221b3f14aedc7e78e6826317869d52fedf9ce42
Nathan Page <33336363+ndpage@users.noreply.github.com>
2024-04-08 22:53:29 -0400

    Fix indentations

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 
-"""Module providing a linear models."""
 from sklearn.linear_model import LinearRegression
+"""Module providing a linear models."""
 
-"""Module providing framework for reading and organizing data."""
 import pandas as pd
+"""Module providing framework for reading and organizing data."""
 
 # Load the dataset (replace this with your dataset)
 data = pd.read_csv("house_prices.csv")
@@ -20,15 +20,15 @@ print(data.head(5))
 
 # Drop unused columns
 X_unused =  [
-  'price',
-  'mainroad',
-  'guestroom',
-  'basement',
-  'hotwaterheating',
-  'airconditioning',
-  'parking',
-  'prefarea',
-  'furnishingstatus'
+       'price',
+       'mainroad',
+       'guestroom',
+       'basement',
+       'hotwaterheating',
+       'airconditioning',
+       'parking',
+       'prefarea',
+       'furnishingstatus'
 ]
 X = data.drop(columns=X_unused)
 y = data['price']
@@ -38,15 +38,15 @@ model = LinearRegression()
 model.fit(X, y)
 
 # House specs: [ area, bedrooms, bathrooms, stories ]
-new_houses = [ 
-  [2500, 3, 2, 1],
-  [5500, 6, 5, 2],
-  [1200, 3, 1.5, 1],
-  [3000, 4, 4, 2],
-  [1000, 2, 2, 1],
-  ]  # Example features for a new house
+new_houses = [
+       [2500, 3, 2, 1],
+       [5500, 6, 5, 2],
+       [1200, 3, 1.5, 1],
+       [3000, 4, 4, 2],
+       [1000, 2, 2, 1],
+]
 predicted_prices = model.predict(new_houses)
 print("\nPredicted price for the new houses: ")
 for house in predicted_prices:
-  print(f"\t{house}")
+       print(f"\t{house}")
 
