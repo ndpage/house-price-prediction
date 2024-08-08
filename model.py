29316c5e31b10df5e5ee5f6da6a66d38fbdb6c7e
Nathan Page <33336363+ndpage@users.noreply.github.com>
2024-08-07 21:30:10 -0400

    Add basic House class, update pylint Python versions

--- model.py
+++ model.py
@@ -4,6 +4,28 @@ from sklearn.linear_model import LinearRegression # Module providing a regressio
 from sklearn.linear_model import LogisticRegression
 import asyncio
 import time
+import locale
+import matplotlib as plt
+
+class House:
+       def __init__(self, 
+              sqft: float,
+              beds: int,
+              baths: float,
+              stories: int,
+       ): 
+              self.sqft = sqft 
+              self.beds = beds
+              self.baths = baths
+              self.stories = stories
+              
+       def get_house_data_list(self) -> list:
+              data = list()
+              data.append(self.sqft)
+              data.append(self.beds)
+              data.append(self.baths)
+              data.append(self.stories)
+              return data
 
 def load_data(csv_file: str) -> DataFrame:
        ''' Load data from a .csv file into a Pandas DataFrame'''
@@ -19,6 +41,19 @@ async def train_model(X, y) -> LinearRegression:
        model.fit(X, y)
        print(f"Model fit complete")
        return model
+
+def print_prices(houses, prices):
+
+       locale.setlocale(locale.LC_ALL, '')  # Use '' for auto, or specify a locale like 'en_US.UTF-8'
+       print("\n[Sqft,bed,bath,floors]\t\tPrice")
+       print("--------------------------------------------")
+       for house, price in zip(houses, prices):
+              formatted_number = locale.format_string("%d", price, grouping=True)
+              print(f'{house}\t  {formatted_number}')
+              
+       print("\n")
+       return 
+
 
 async def main():
        start_time = time.time()
@@ -59,13 +94,21 @@ async def main():
               [3000, 4, 4, 2],
               [1000, 2, 2, 1],
        ]
+       
+       house1 = House(1800, 3, 1.5, 1)
+       new_houses.append(house1.get_house_data_list())
+       print(new_houses)
        predicted_prices = model.predict(new_houses)
-       print("\n[Sqft,bed,bath,floors]\t\tPrice")
-       print("--------------------------------------------")
-       for house, price in zip(new_houses, predicted_prices):
-              print(f"{house}:\t\t$ {float(price):0.2f}")
-       print("\n")
+       
+       
+       # print("\n[Sqft,bed,bath,floors]\t\tPrice")
+       # print("--------------------------------------------")
+       # for house, price in zip(new_houses, predicted_prices):
+       #        print(f"{house}:\t\t$ {float(price):0.2f}")
+       # print("\n")
+       print(print_prices(new_houses, predicted_prices))
        end_time = time.time()
+       
 
        execution_time = end_time - start_time
        print(f"Execution time: {execution_time} seconds\n")
